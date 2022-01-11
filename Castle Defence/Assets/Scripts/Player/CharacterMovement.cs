c3a11c6d59e811c0018e5184a6ba033e75d2d80d
bilasyurii <bilas.yurii@gmail.com>
2022-01-12 01:29:26 +0200

    New character controller basis.

--- Castle Defence/Assets/Scripts/Player/CharacterMovement.cs
+++ Castle Defence/Assets/Scripts/Player/CharacterMovement.cs
@@ -1,76 +1,203 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
 public class CharacterMovement : MonoBehaviour
 {
-    [Header("General")]
     [SerializeField] private Transform _camera;
-
-    [Header("Movement")]
-    [SerializeField] private float _jumpHeight = 3f;
-    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _floorOffsetY = 0.9f;
     [SerializeField] private float _gravity = -40f;
+    [SerializeField] private float _jumpHeight = 3f;
+    [SerializeField] private float _movementSpeed = 10f;
+    [SerializeField] private float _rotationSpeed = 10f;
 
-    [HeaderAttribute("Rotation")]
-    [SerializeField] private float _turnSmoothTime = 0.1f;
+    private Rigidbody _rigidbody;
 
-    private CharacterController _controller;
+    private float _inputX;
+    private float _inputY;
 
-    private float velocityY = 0f;
-    private bool _isGrounded = false;
-    private float _turnSmoothVelocity;
+    private Vector3 _movementDirection;
+    private RaycastHit _slopeHit;
+
+    [ReadOnly] [SerializeField] private float _velocityY;
+    [ReadOnly] [SerializeField] private bool _isGrounded;
+    [ReadOnly] [SerializeField] private Vector3 _floorPosition;
+    [ReadOnly] [SerializeField] private Vector3 _combinedRaycast;
+    [ReadOnly] [SerializeField] private Vector3 _combinedSlopeNormal;
+
+    [SerializeField] private float _raycastOffsetVertical = 0f;
+    [SerializeField] private float _raycastOffsetHorizontal = 0.5f;
+    [SerializeField] private float _floorRaycastDistance = 1.2f;
+    [SerializeField] private float _groundCheckRaycastDistance = 1f;
 
     private void Start()
     {
-        _controller = GetComponent<CharacterController>();
+        _rigidbody = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void Update()
     {
-        _isGrounded = _controller.isGrounded;
+        _inputX = Input.GetAxisRaw("Horizontal");
+        _inputY = Input.GetAxisRaw("Vertical");
+        //_inputY = 1;
 
-        if (_isGrounded && velocityY < 0f)
+        var movementForward = _camera.forward * _inputY;
+        var movementSide = _camera.right * _inputX;
+        var movementCombined = movementForward + movementSide;
+        var movementNormalized = movementCombined.normalized;
+
+        _movementDirection = new Vector3(movementNormalized.x, 0f, movementNormalized.z);
+
+        //  if there is any movement, interpolate rotation of a character to look at movement direction
+        if (_movementDirection != Vector3.zero)
         {
-            velocityY = -2f;
-        }
-
-        var horizontal = Input.GetAxisRaw("Horizontal");
-        var vertical = Input.GetAxisRaw("Vertical");
-        var direction = new Vector3(horizontal, 0f, vertical).normalized;
-        var dt = Time.deltaTime;
-
-        var movementVector = Vector3.zero;
-
-        if (direction.sqrMagnitude >= 0.01f)
-        {
-            var targetAngleRad = Mathf.Atan2(direction.x, direction.z);
-            var targetAngleDeg = targetAngleRad * Mathf.Rad2Deg;
-            var cameraAngle = _camera.eulerAngles.y;
-            var targetAngle = targetAngleDeg + cameraAngle;
-            var currentAngle = transform.eulerAngles.y;
-            var angle = Mathf.SmoothDampAngle(
-                currentAngle,
-                targetAngle,
-                ref _turnSmoothVelocity,
-                _turnSmoothTime
-            );
-
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            var moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            var moveDirectionNormalized = moveDirection.normalized;
-            var movement = _speed * dt;
-            movementVector = moveDirectionNormalized * movement;;
+            var rotationAmount = Time.deltaTime * _rotationSpeed;
+            var targetRotation = Quaternion.LookRotation(_movementDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationAmount);
         }
 
         if (Input.GetButton("Jump") && _isGrounded)
         {
-            velocityY = Mathf.Sqrt(_jumpHeight * -2f * _gravity);
+            print("jump");
+            _velocityY = Mathf.Sqrt(_jumpHeight * -2f * _gravity);
+            var velocity = _rigidbody.velocity;
+            _rigidbody.velocity = new Vector3(velocity.x, _velocityY, velocity.z);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // raycasting downwards to check, if character is on ground
+        _isGrounded = RaycastFloor(0f, 0f, _groundCheckRaycastDistance, out _);
+
+        Vector3 velocity;
+
+        FindFloorParameters();
+
+        var root = transform.TransformPoint(Vector3.down * _floorOffsetY);
+        Debug.DrawLine(root, root + _combinedSlopeNormal * 4f, Color.yellow);
+
+        // setting rigidbody's velocity
+        if (_isGrounded && _combinedSlopeNormal != Vector3.zero)
+        {
+            var slopeMovementDirection = Vector3.ProjectOnPlane(_movementDirection, _combinedSlopeNormal);
+            velocity = slopeMovementDirection * _movementSpeed;
+
+            Debug.DrawLine(root, root + slopeMovementDirection * 4f, Color.cyan);
+        }
+        else
+        {
+            velocity = _movementDirection * _movementSpeed;
         }
 
-        velocityY += _gravity * dt;
-        movementVector.y += velocityY * dt;
+        // if character is in mid - air(not grounded), then add velocity downwards
+        if (_isGrounded == false)
+        {
+            _velocityY += _gravity * Time.fixedDeltaTime;
+            velocity.y += _velocityY;
+        }
 
-        _controller.Move(movementVector);
+        _rigidbody.velocity = velocity;
+
+        if (_isGrounded)
+        {
+            FindFloorParameters();
+            // calculating position of character as if it was on floor
+            var positionY = _combinedRaycast.y + _floorOffsetY;
+            var position = _rigidbody.position;
+            _floorPosition = new Vector3(position.x, positionY, position.z);
+
+            // sticking character to floor to fix jittering
+            if (_floorPosition != position)
+            {
+                velocity.y = 0;
+                _velocityY = 0f;
+
+                _rigidbody.velocity = velocity;
+                _rigidbody.position = _floorPosition;
+            }
+        }
+    }
+
+    private bool CheckSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out _slopeHit, _floorRaycastDistance))
+        {
+            return _slopeHit.normal != Vector3.up;
+        }
+
+        return false;
+    }
+
+    // make a raycast to find intersection with floor
+    private bool RaycastFloor(float offsetX, float offsetZ, float length, out RaycastHit hit)
+    {
+        // getting raycast origin in world coordinates
+        var raycastFloorPosition = transform.TransformPoint(offsetX, _raycastOffsetVertical, offsetZ);
+
+        // for debugging
+        if (Mathf.Approximately(length, _groundCheckRaycastDistance) == false)
+        {
+            Debug.DrawLine(raycastFloorPosition, raycastFloorPosition + Vector3.down * length, Color.magenta);
+        }
+
+        // sending raycast
+        return Physics.Raycast(raycastFloorPosition, Vector3.down, out hit, length);
+    }
+
+    // calculate average of 5 raycasts
+    private void FindFloorParameters()
+    {
+        // reset combined raycast & slope normal vectors
+        _combinedRaycast = Vector3.zero;
+        _combinedSlopeNormal = Vector3.zero;
+
+        // raycast in the center
+        CombineRaycastTo(0f, 0f);
+
+        // count of raycasts, that have hit the ground
+        // raycast in center 
+        var hitCount = 1;
+
+        // 4 side raycasts
+        if (CombineRaycastTo(_raycastOffsetHorizontal, 0f))
+        {
+            ++hitCount;
+        }
+
+        if (CombineRaycastTo(-_raycastOffsetHorizontal, 0f))
+        {
+            ++hitCount;
+        }
+
+        if (CombineRaycastTo(0f, _raycastOffsetHorizontal))
+        {
+            ++hitCount;
+        }
+
+        if (CombineRaycastTo(0f, -_raycastOffsetHorizontal))
+        {
+            ++hitCount;
+        }
+
+        _combinedRaycast /= hitCount;
+        _combinedSlopeNormal /= hitCount;
+    }
+
+    // make a raycast and combine hit point with the _combinedRaycast vector
+    // returns boolean, that indicates, whether the cast has hit ground
+    private bool CombineRaycastTo(float offsetX, float offsetZ)
+    {
+        var hasHit = RaycastFloor(offsetX, offsetZ, _floorRaycastDistance, out _slopeHit);
+
+        if (hasHit)
+        {
+            _combinedRaycast += _slopeHit.point;
+            _combinedSlopeNormal += _slopeHit.normal;
+            return true;
+        }
+
+        return false;
     }
 }
---
+++ Castle Defence/Assets/Scripts/Player/CharacterMovementOld.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+public class CharacterMovementOld : MonoBehaviour
+{
+    [Header("General")]
+    [SerializeField] private Transform _camera;
+
+    [Header("Movement")]
+    [SerializeField] private float _jumpHeight = 3f;
+    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _gravity = -40f;
+
+    [HeaderAttribute("Rotation")]
+    [SerializeField] private float _turnSmoothTime = 0.1f;
+
+    private CharacterController _controller;
+
+    private float velocityY = 0f;
+    private bool _isGrounded = false;
+    private float _turnSmoothVelocity;
+
+    private void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        _isGrounded = _controller.isGrounded;
+
+        if (_isGrounded && velocityY < 0f)
+        {
+            velocityY = -2f;
+        }
+
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+        var direction = new Vector3(horizontal, 0f, vertical).normalized;
+        var dt = Time.deltaTime;
+
+        var movementVector = Vector3.zero;
+
+        if (direction.sqrMagnitude >= 0.01f)
+        {
+            var targetAngleRad = Mathf.Atan2(direction.x, direction.z);
+            var targetAngleDeg = targetAngleRad * Mathf.Rad2Deg;
+            var cameraAngle = _camera.eulerAngles.y;
+            var targetAngle = targetAngleDeg + cameraAngle;
+            var currentAngle = transform.eulerAngles.y;
+            var angle = Mathf.SmoothDampAngle(
+                currentAngle,
+                targetAngle,
+                ref _turnSmoothVelocity,
+                _turnSmoothTime
+            );
+
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            var moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            var moveDirectionNormalized = moveDirection.normalized;
+            var movement = _speed * dt;
+            movementVector = moveDirectionNormalized * movement;;
+        }
+
+        if (Input.GetButton("Jump") && _isGrounded)
+        {
+            velocityY = Mathf.Sqrt(_jumpHeight * -2f * _gravity);
+        }
+
+        velocityY += _gravity * dt;
+        movementVector.y += velocityY * dt;
+
+        _controller.Move(movementVector);
+    }
+}
